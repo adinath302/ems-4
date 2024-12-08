a3d32845e7165d2319e14722a59f391efa13939a
Adinath Gaware <139313629+adinath302@users.noreply.github.com>
2024-12-08 22:27:08 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Login from "./Component/Auth/Login";
+import EmployeeDashboard from "./Component/Dashboard/EmployeeDashboard";
+import AdminDashboard from "./Component/Dashboard/AdminDashboard";
+
+const App = () => {
+  return (
+    <>
+      {/* <Login /> */}
+      {/* <EmployeeDashboard /> */}
+      <AdminDashboard />
+    </>
+  );
+};
+
+export default App;
